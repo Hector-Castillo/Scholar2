97ecd55a17ee6cf7364d6ddf8d750c1503873105
ESTUDIANTE <estudiante@ul.edu.co>
2019-11-15 13:36:51 -0500

    Parcial de Programacion

---
+++ Mapa/src/mapa/Mapa.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mapa;
+
+/**
+ *
+ * @author estudiante
+ */
+public class Mapa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+    DatosEstudiante VD =new DatosEstudiante();
+    VD.setVisible(true);
+    
+    }
+    
+}
